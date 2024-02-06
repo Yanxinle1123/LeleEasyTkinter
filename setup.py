b9b2e8a38945b84d86ae0c9b1cd0bf612be85f66
yanxinle <1020121123@qq.com>
2024-02-06 20:50:52 +0800

    添加 README.md 和 setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="LeleSR",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=[
+        "SpeechRecognition",
+        "langid",
+        "pyaudio",
+    ],
+    author="YanXinle",
+    author_email="1020121123@qq.com",
+    description="语音识别的简化库",
+    url="https://github.com/Yanxinle1123/LeleEasyTkinter",
+)
