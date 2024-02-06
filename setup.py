d24d49d409b35058fdbaae4cefc406915381f74e
yanxinle <1020121123@qq.com>
2024-02-06 20:53:22 +0800

    更新 setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="LeleSR",
-    version="1.0",
+    name="LeleET",
+    version="0.1",
     packages=find_packages(),
-    install_requires=[
-        "SpeechRecognition",
-        "langid",
-        "pyaudio",
-    ],
     author="YanXinle",
     author_email="1020121123@qq.com",
     description="语音识别的简化库",
