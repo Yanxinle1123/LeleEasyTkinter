a77616f7f28cb7c54a267af72d1dd04ff8e90fe3
yanxinle <1020121123@qq.com>
2024-02-13 19:25:14 +0800

    更新setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="LeleET",
-    version="0.1",
+    name="LeleEasyTkinter",
+    version="0.0.1",
     packages=find_packages(),
     author="YanXinle",
     author_email="1020121123@qq.com",
-    description="语音识别的简化库",
+    description="tkinter库的简化版",
     url="https://github.com/Yanxinle1123/LeleEasyTkinter",
 )
