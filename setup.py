8db2398768f0339efb2deb239401a21d507ccfee
yanxuewei <yanxuewei@163.com>
2024-02-14 21:00:51 +0800

    #、优化代码

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="LeleEasyTkinter",
-    version="0.0.3",
+    version="0.0.4",
     packages=find_packages(),
     author="YanXinle",
     author_email="1020121123@qq.com",
