25ec399418a2e7a67582df26239ec3091dcbbce3
yanxinle <1020121123@qq.com>
2024-05-28 17:55:08 +0800

    更新代码, 修复bug, 准备打包为1.0.9版本

--- LeleEasyTkinter/easy_fade_animation.py
+++ LeleEasyTkinter/easy_fade_animation.py
@@ -1,22 +1,36 @@
 import tkinter as tk
 
-from easy_auto_window import EasyAutoWindow
-from easy_button import EasyButton
+from LeleEasyTkinter.easy_auto_window import EasyAutoWindow
+from LeleEasyTkinter.easy_button import EasyButton
 
 
-def fade_in(window, alpha=0):
-    if alpha >= 1:
-        return
-    window.attributes("-alpha", alpha)
-    window.after(5, fade_in, window, alpha + 0.01)
+def fade_in(window, alpha=0, ms=0):
+    # 初始化窗口的透明度为0
+    if alpha == 0:
+        window.attributes('-alpha', 0)
 
-
-def fade_out(window, alpha=1):
-    if alpha <= 0:
-        window.destroy()
-    else:
+    def start_fade_in_animation():
+        nonlocal alpha
+        if alpha >= 1:
+            return
+        alpha += 0.01
         window.attributes("-alpha", alpha)
-        window.after(5, fade_out, window, alpha - 0.01)
+        window.after(5, start_fade_in_animation)
+
+    window.after(ms, start_fade_in_animation)
+
+
+def fade_out(window, alpha=1, ms=0):
+    def start_fade_out_animation():
+        nonlocal alpha
+        if alpha <= 0:
+            window.destroy()
+        else:
+            alpha -= 0.01
+            window.attributes("-alpha", alpha)
+            window.after(5, start_fade_out_animation)
+
+    window.after(ms, start_fade_out_animation)
 
 
 if __name__ == '__main__':
@@ -28,5 +42,5 @@ if __name__ == '__main__':
     EasyAutoWindow(root, window_title="EasyAutoWindow", window_width_value=400, window_height_value=300, adjust_y=False,
                    adjust_x=False)
     EasyButton(root, "退出", expand=tk.YES, width=10, height=1, font_size=12, cmd=quit_window)
-    root.after(500, fade_in(root))
+    root.after(500, fade_in(root, ms=500))
     root.mainloop()
