5dbc00f3765fe60c3c68c07a53630d0f31b23c9f
yanxinle <1020121123@qq.com>
2024-06-08 12:31:58 +0800

    更新弹窗功能, 打包为1.1.2版本

--- LeleEasyTkinter/easy_warning_windows.py
+++ LeleEasyTkinter/easy_warning_windows.py
@@ -1,27 +1,31 @@
+import tkinter as tk
 import tkinter.messagebox as messagebox
 from tkinter import simpledialog
 
+from easy_auto_window import EasyAutoWindow
+
 
 class EasyWarningWindows:
-    def __init__(self, title="警告", message="这是一个警告消息"):
+    def __init__(self, parent, title="警告", message="这是一个警告消息"):
+        self._parent = parent
         self._title = title
         self._message = message
 
     def show_warning(self):
         if self._title == "警告":
-            messagebox.showwarning(self._title, self._message)
+            messagebox.showwarning(self._title, self._message, parent=self._parent)
         elif self._title == "信息":
-            messagebox.showinfo(self._title, self._message)
+            messagebox.showinfo(self._title, self._message, parent=self._parent)
         elif self._title == "错误":
-            messagebox.showerror(self._title, self._message)
+            messagebox.showerror(self._title, self._message, parent=self._parent)
         elif self._title == "询问":
-            result = messagebox.askquestion(self._title, self._message)
+            result = messagebox.askquestion(self._title, self._message, parent=self._parent)
             return result
         elif self._title == "是/否":
-            result = messagebox.askyesno(self._title, self._message)
+            result = messagebox.askyesno(self._title, self._message, parent=self._parent)
             return result
         elif self._title == "输入框":
-            result = simpledialog.askstring(self._title, self._message)
+            result = simpledialog.askstring(self._title, self._message, parent=self._parent)
             return result
 
     def get_title(self):
@@ -31,20 +35,26 @@ class EasyWarningWindows:
         return self._message
 
 
-def demo_warning():
-    EasyWarningWindows("警告", "这是一个警告").show_warning()
-    EasyWarningWindows("信息", "这是一个信息").show_warning()
-    EasyWarningWindows("错误", "这是一个错误").show_warning()
+def demo_warning(root):
+    EasyWarningWindows(root, "警告", "这是一个警告").show_warning()
+    EasyWarningWindows(root, "信息", "这是一个信息").show_warning()
+    EasyWarningWindows(root, "错误", "这是一个错误").show_warning()
 
-    askquestion_result = EasyWarningWindows("询问", "这是一条询问").show_warning()
+    askquestion_result = EasyWarningWindows(root, "询问", "这是一条询问").show_warning()
     print(askquestion_result)
 
-    askyesno_result = EasyWarningWindows("是/否", "这是一个选择").show_warning()
+    askyesno_result = EasyWarningWindows(root, "是/否", "这是一个选择").show_warning()
     print(askyesno_result)
 
-    askstring_result = EasyWarningWindows("输入框", "这是一个输入框").show_warning()
+    askstring_result = EasyWarningWindows(root, "输入框", "这是一个输入框").show_warning()
     print(askstring_result)
 
 
 if __name__ == '__main__':
-    demo_warning()
+    root = tk.Tk()
+    EasyAutoWindow(root)
+    root.attributes('-topmost', 'true')
+
+    demo_warning(root)
+
+    root.mainloop()
