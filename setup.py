be0c0672e71968125a4c6e68c7e78ee94b2189e7
yanxinle <1020121123@qq.com>
2024-07-05 13:29:15 +0800

    更新  -修复了一些已知问题  -将 LeleEasyTkinter 升级为 1.4.8 版本

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="LeleEasyTkinter",
-    version="1.4.7",
+    version="1.4.8",
     packages=find_packages(),
     author="YanXinle",
     author_email="1020121123@qq.com",
