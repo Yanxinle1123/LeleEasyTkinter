80d82ce5f6692a7a8c510e815f077de97d6e40c9
yanxinle <1020121123@qq.com>
2024-07-05 13:24:18 +0800

    更新 更改移动窗口的方法, 并将 LeleEasyTkinter 升级为 1.4.7 版本

--- LeleEasyTkinter/easy_mobile_animation.py
+++ LeleEasyTkinter/easy_mobile_animation.py
@@ -2,10 +2,11 @@ import math
 import random
 import tkinter
 
+from LeleEasyTkinter.easy_auto_window import EasyAutoWindow
 from LeleEasyTkinter.easy_button import EasyButton
 
 
-def move_window_to(window, target_x, target_y, step=1):
+def move_window_to(window, target_x, target_y, steps=100):
     # 获取当前窗口的位置和大小
     geometry = window.geometry().split('+')
     current_x = int(geometry[1])
@@ -15,24 +16,15 @@ def move_window_to(window, target_x, target_y, step=1):
     distance_x = target_x - current_x
     distance_y = target_y - current_y
 
-    # 计算总距离和每一步的距离
-    total_distance = math.sqrt(distance_x ** 2 + distance_y ** 2)
-    steps = int(total_distance / step)
-
-    # 如果 steps 为 0，直接将窗口移动到目标位置并返回
-    if steps == 0:
-        window.geometry(f"+{target_x}+{target_y}")
-        return
-
-    # 计算每一步的 x 和 y 方向上的距离
-    step_x = distance_x / steps
-    step_y = distance_y / steps
-
     # 逐像素移动窗口
-    for _ in range(steps):
-        current_x += step_x
-        current_y += step_y
-        window.geometry(f"+{int(current_x)}+{int(current_y)}")
+    for i in range(steps + 1):
+        ratio = i / steps
+        factor = 0.5 - 0.5 * math.cos(ratio * math.pi)  # 使用正弦函数计算移动因子
+
+        x = int(current_x + distance_x * factor)
+        y = int(current_y + distance_y * factor)
+
+        window.geometry(f"+{x}+{y}")
         window.update()
 
     # 确保窗口移动到准确的目标位置
@@ -42,18 +34,14 @@ def move_window_to(window, target_x, target_y, step=1):
 if __name__ == '__main__':
     root = tkinter.Tk()
     root.geometry("200x200")
-    root.resizable(False, False)
+    EasyAutoWindow(root, window_title="动画演示", window_width_value=210, window_height_value=100, adjust_x=False,
+                   adjust_y=False)
 
-    # 获取屏幕的大小
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
-
-    # 创建一个按钮，点击时将窗口移动到屏幕内的一个随机位置
     EasyButton(root, "移动窗口",
                cmd=lambda: move_window_to(root,
-                                          target_x=random.randint(0, screen_width - 500),
-                                          target_y=random.randint(0, screen_height - 500),
-                                          step=20),
+                                          target_x=random.randint(200, 1000),
+                                          target_y=random.randint(200, 800),
+                                          steps=100),
                width=10, height=1, font_size=12, expand=tkinter.YES)
 
     root.mainloop()
