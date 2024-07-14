c21c30f0a4b2c47d20936c3f9a015a07aa0ff7e0
yanxinle <1020121123@qq.com>
2024-07-14 16:08:04 +0800

    更新  -添加弹窗动画  -将 LeleEasyTkinter 升级为 1.6.0 版本

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="LeleEasyTkinter",
-    version="1.5.9",
+    version="1.6.0",
     packages=find_packages(),
     author="YanXinle",
     author_email="1020121123@qq.com",
