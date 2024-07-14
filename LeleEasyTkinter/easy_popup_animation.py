1752ab5a60bc4ab7a99e7e0b89e35ae63c120db4
yanxinle <1020121123@qq.com>
2024-07-14 16:44:55 +0800

    更新  -添加新功能  -将 LeleEasyTkinter 升级为 1.6.3 版本

--- LeleEasyTkinter/easy_popup_animation.py
+++ LeleEasyTkinter/easy_popup_animation.py
@@ -1,6 +1,12 @@
 import tkinter
 
 from LeleEasyTkinter.easy_auto_window import EasyAutoWindow
+from LeleEasyTkinter.easy_fade_animation import fade_out
+from LeleEasyTkinter.easy_label import EasyLabel
+
+
+def close_window(event=None):
+    fade_out(root)
 
 
 def center_window(root):
@@ -24,7 +30,7 @@ def cubic_easing(t):
     return 4 * t * t * t + 1
 
 
-def animate_resize_window(root, target_width, target_height, steps=100, way='ordinary'):
+def animate_resize_window(root, target_width, target_height, steps=100, way='ordinary', center=False):
     root.update()
 
     # 获取当前窗口大小和位置
@@ -46,14 +52,19 @@ def animate_resize_window(root, target_width, target_height, steps=100, way='ord
         new_y = current_y - (target_height - current_height) * eased_t / 2
 
         root.geometry(f"{int(new_width)}x{int(new_height)}+{int(new_x)}+{int(new_y)}")
-        center_window(root)
+        if center:
+            center_window(root)
         root.update()  # 更新窗口
 
 
 root = tkinter.Tk()
+
 EasyAutoWindow(root, window_title="动画演示", window_width_value=1, window_height_value=1, adjust_x=False,
-               adjust_y=False)
+               adjust_y=False, window_x_value=700, window_y_value=500)
 
-animate_resize_window(root, 1000, 600, 50, "ordinary")
+EasyLabel(root, text="动画演示", expand=True, font_size=100)
 
+animate_resize_window(root, 1000, 600, 50, "ordinary", False)
+
+root.protocol("WM_DELETE_WINDOW", close_window)
 root.mainloop()
