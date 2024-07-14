767fae4f0bf011a50c61d076b723a3699cd70663
yanxinle <1020121123@qq.com>
2024-07-14 16:06:11 +0800

    更新  -添加弹窗动画  -将 LeleEasyTkinter 升级为 1.6.0 版本

---
+++ LeleEasyTkinter/easy_popup_animation.py
@@ -0,0 +1,56 @@
+import tkinter
+
+from LeleEasyTkinter.easy_auto_window import EasyAutoWindow
+
+
+def center_window(root):
+    width = root.winfo_width()
+    height = root.winfo_height()
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+
+    x = (screen_width - width) // 2
+    y = (screen_height - height) // 2 - 20
+
+    root.focus_set()
+    root.lift()
+    root.geometry(f"+{x}+{y}")
+
+
+def animate_resize_window(root, target_width, target_height, steps=100):
+    root.geometry("1x1")
+
+    # 获取当前窗口大小和位置
+    geometry = root.geometry()
+    current_width = int(geometry.split('x')[0])
+    rest = geometry.split('x')[1].split('+')
+    current_height = int(rest[0])
+    current_x = int(rest[1])
+    current_y = int(rest[2])
+
+    # 计算每步的宽度和高度增量
+    width_step = (target_width - current_width) / steps
+    height_step = (target_height - current_height) / steps
+
+    # 计算每步的位置增量
+    x_step = width_step / 2
+    y_step = height_step / 2
+
+    # 逐步改变窗口大小和位置
+    for i in range(steps):
+        current_width += width_step
+        current_height += height_step
+        current_x -= x_step
+        current_y -= y_step
+        root.geometry(f"{int(current_width)}x{int(current_height)}+{int(current_x)}+{int(current_y)}")
+        center_window(root)
+        root.update()  # 更新窗口
+
+
+root = tkinter.Tk()
+EasyAutoWindow(root, window_title="动画演示", window_width_value=1, window_height_value=1, adjust_x=False,
+               adjust_y=False)
+
+animate_resize_window(root, 1000, 600, 20)
+
+root.mainloop()
--- LeleEasyTkinter/test.py
+++ LeleEasyTkinter/test.py
@@ -4,9 +4,16 @@ from LeleEasyTkinter.easy_auto_window import EasyAutoWindow
 from LeleEasyTkinter.easy_button import EasyButton
 
 root = tkinter.Tk()
-EasyAutoWindow(root, window_title="Button", window_width_value=150, window_height_value=80, adjust_x=True,
+EasyAutoWindow(root, window_title="Button", window_width_value=300, window_height_value=80, adjust_x=True,
                adjust_y=True)
 
-EasyButton(root, "Button", expand=tkinter.YES, fill=tkinter.BOTH, auto_resize_font=True)
+EasyButton(root, text="退出", fill=tkinter.BOTH, expand=True, side=tkinter.LEFT, height=2, auto_resize_font=True,
+           value=3)
+
+EasyButton(root, text="设置", fill=tkinter.BOTH, expand=True, side=tkinter.LEFT, height=2, auto_resize_font=True,
+           value=3)
+
+EasyButton(root, text="使用方法", fill=tkinter.BOTH, expand=True, side=tkinter.LEFT, height=2,
+           auto_resize_font=True, value=3)
 
 root.mainloop()
